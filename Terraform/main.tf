4f8637c09f3b91458fd46a756f56788214374973
Yang Y Lu <Yang_Y_Lu@mckinsey.com>
2025-11-24 09:45:05 +0800

    format

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,10 @@
 # ---------------------- VPC Endpoint for Secrets Manager ----------------------
 
 resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.${var.region}.secretsmanager"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = var.private_subnets
+  vpc_id             = var.vpc_id
+  service_name       = "com.amazonaws.${var.region}.secretsmanager"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = var.private_subnets
   security_group_ids = [aws_security_group.crawler.id]
 
   private_dns_enabled = true
