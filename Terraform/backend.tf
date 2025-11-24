e584b0e81879f928513c2c71754f262d9dac742b
Yang Y Lu <Yang_Y_Lu@mckinsey.com>
2025-11-24 11:23:29 +0800

    format

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "trandrader-tf-backend-s3"
-    key    = "terraform.tfstate"
-    region = "ap-southeast-1"
+    bucket  = "trandrader-tf-backend-s3"
+    key     = "terraform.tfstate"
+    region  = "ap-southeast-1"
     encrypt = true
   }
 }
